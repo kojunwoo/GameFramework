3e738ca47c2d60b4e07627e84c11ad0adca3c21c
kojunwoo <20171103@vision.hoseo.edu>
2018-10-11 17:59:03 +0900

    10월11일 PP07.TextureManager

--- PP07.TextureManager/Game.h
+++ PP07.TextureManager/Game.h
@@ -1,6 +1,9 @@
 #pragma once
+
 #include <iostream>
 #include "SDL.h"
+#include "TextureManager.h"
+
 class Game
 {
 public:
@@ -12,11 +15,15 @@ public:
 	void handleEvents();
 	void clean();
 	bool running() { return m_bRunning; }
+
+
 private:
-	SDL_Texture * m_pTexture;
-	SDL_Rect m_sourceRectangle;
-	SDL_Rect m_destinationRectangle;
+	//SDL_Texture * m_pTexture;
+	//SDL_Rect m_sourceRectangle;
+	//SDL_Rect m_destinationRectangle;
 	SDL_Window * m_pWindow;
 	SDL_Renderer*m_pRenderer;
+	TextureManager m_textureManager;
+	int m_currentFrame;
 	bool m_bRunning;
 };
