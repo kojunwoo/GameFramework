ba5a9a295031ac800a2840a1732138b7f6c2698c
kojunwoo <20171103@vision.hoseo.edu>
2018-10-18 17:36:48 +0900

    10월 18일 PP09.Inheritance

---
+++ PP09.Inheritance/Game.cpp
@@ -0,0 +1,86 @@
+#include "Game.h"
+
+bool Game::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+
+
+		m_pWindow = SDL_CreateWindow("PP09.Inheritance",
+
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			640, 480, SDL_WINDOW_SHOWN);
+
+		if (m_pWindow != 0) {
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+	}
+	else {
+
+		m_bRunning = false;
+		return false;
+	}
+
+	m_go.load(100, 100, 43, 110, "animate");
+	m_player.load(300, 300, 43, 110, "animate");
+
+	TheTextureManager::Instance()->load("assets/dog sprite.png", "animate", m_pRenderer);
+	//TheTextureManager::Instance()->load("assets/dog sprite.png", "animate", m_pRenderer);
+	//TheTextureManager::Instance()->load("assets/New Piskel.png", "background", m_pRenderer);
+
+	SDL_SetRenderDrawColor(m_pRenderer, 200, 200, 255, 100);
+
+
+	m_bRunning = true;
+	return true;
+}
+
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	m_go.draw(m_pRenderer);
+	m_player.draw(m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+
+	/*SDL_RenderClear(m_pRenderer);
+
+	TheTextureManager::Instance()->draw("background", 0, 0, 640, 480, m_pRenderer);
+	TheTextureManager::Instance()->drawFrame("animate", 300, 290, 43, 110, 1,
+		m_currentFrame, m_pRenderer);
+	
+	SDL_RenderPresent(m_pRenderer);*/
+}
+
+void Game::update() {
+	//m_currentFrame = int(((SDL_GetTicks() / 100) % 4));
+	//m_currentFrame2 = int(((SDL_GetTicks() / 70) % 6));
+
+	m_go.update();
+	m_player.update();
+}
+
+void Game::clean()
+{
+	std::cout << "cleaning game\n";
+
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
---
+++ PP09.Inheritance/Game.h
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include <iostream>
+#include "SDL.h"
+#include "TextureManager.h"
+#include "GameObject.h"
+#include "Player.h"
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+	GameObject m_go;
+	Player m_player;
+
+
+private:
+	//SDL_Texture * m_pTexture;
+	//SDL_Rect m_sourceRectangle;
+	//SDL_Rect m_destinationRectangle;
+	SDL_Window * m_pWindow;
+	SDL_Renderer*m_pRenderer;
+	//TextureManager m_textureManager;
+	int m_currentFrame;
+	int m_currentFrame2;
+	bool m_bRunning;
+};
