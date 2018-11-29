6cc965f438ffaad6b4f6c08ea40bc0107afe8ce6
kojunwoo <20171103@vision.hoseo.edu>
2018-11-29 16:46:54 +0900

    11월29일 PP17.PlayState

---
+++ PP17.PlayState/Enemy.cpp
@@ -0,0 +1,42 @@
+#include "Enemy.h"
+#include "InputHandler.h"
+
+Enemy::Enemy(const LoaderParams* pParams) :
+	SDLGameObject(pParams)
+{
+}
+
+void Enemy::draw()
+{
+	SDLGameObject::draw();
+}
+
+void Enemy::update()
+{
+	m_velocity.setX(0);
+	m_velocity.setY(0);
+	handleInput();
+
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 7));
+	SDLGameObject::update();
+
+	m_position.setX(m_position.getX() + 1);
+	m_position.setY(m_position.getY() + 1);
+
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 7));
+}
+
+void Enemy::clean()
+{
+}
+
+void Enemy::handleInput()
+{
+	if (TheInputHandler::Instance()->getMouseButtonState(LEFT))
+	{
+		m_velocity.setX(1);
+	}
+
+	Vector2D* vec = TheInputHandler::Instance()->getMousePosition();
+	m_velocity = (*vec - m_position) / 100;
+}
